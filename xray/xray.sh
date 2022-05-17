d4a1f0c7a12d9485b07914fd29ba60e28cdf84da
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-05-18 01:32:44 +0800

    Add files via upload

---
+++ xray/xray.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#性能优化
+echo 500000 >/proc/sys/kernel/pid_max
+echo 500000 >/proc/sys/kernel/threads-max
+echo 500000 >/proc/sys/vm/max_map_count
+echo 500000 >/proc/sys/fs/file-max
+ulimit -SHn 10240
+ulimit -SHs unlimited
+modprobe ip_conntrack
+echo 1000000 >/proc/sys/net/nf_conntrack_max
+lsmod |grep conntrack
+
+#下载安装XRAY
+mkdir /etc/xray
+wget --no-check-certificate -O /etc/xray/Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v1.5.5/Xray-linux-64.zip
+unzip -o /etc/xray/Xray-linux-64.zip
+mkdir /etc/xray/bin
+wget --no-check-certificate -O /etc/xray/bin/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/amazon/socks5/config.json
+chmod +x /etc/xray
+wget --no-check-certificate -O /lib/systemd/system/qd.service https://raw.githubusercontent.com/hkshiqi/x-ui/main/amazon/socks5/qd.service
+
+#安装启动BBR
+wget --no-check-certificate -O /etc/xray/bbr.sh https://raw.githubusercontent.com/hkshiqi/x-ui/main/installbbr/bbr && chmod +x /etc/xray/bbr.sh && ./bbr.sh
+sudo systemctl daemon-reload
+systemctl enable qd.service
