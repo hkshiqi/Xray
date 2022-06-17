3f58c85497f4794e3095ccf57534586707b24952
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-06-17 12:16:03 +0800

    Update qd.sh

--- xray/qd.sh
+++ xray/qd.sh
@@ -8,6 +8,9 @@ ifconfig eth0:6 10.0.0.10 up
 ifconfig eth0:7 10.0.0.11 up
 ifconfig eth0:8 10.0.0.12 up
 ifconfig eth0:9 10.0.0.13 up
-
+ifconfig eth0:1 10.0.0.14 up
+ifconfig eth0:1 10.0.0.15 up
+ifconfig eth0:1 10.0.0.16 up
+ifconfig eth0:1 10.0.0.17 up
 #启动Xray
 runuser -l root -c 'nohup /etc/xray/xray -c /etc/xray/bin/config.json &'
